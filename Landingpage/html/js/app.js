6fdce462bba6cad37b955d16f606d24c90a53cff
lucius02 <54938206+lucius02@users.noreply.github.com>
2020-11-25 09:58:02 +0100

    Add files via upload

--- Landingpage/html/js/app.js
+++ Landingpage/html/js/app.js
@@ -1,6 +1,7 @@
-const cardList = document.getElementById('cardList');
+
 const searchBar = document.getElementById('searchBar');
 let cardRecommended = [];
+let cardUploaded = [];
 
 searchBar.addEventListener('keyup', (e) => {
     const searchString = e.target.value;
@@ -24,20 +25,48 @@ const loadCharacters = async () => {
     }
 };
 
-const displayRecommended = (Recommended) => {
-    const htmlString = Recommended
+const displayRecommended = (Cards) => {
+    const htmlString = Cards
         .map((Card) => {
             return `
             <ul class="cardListRecommended">
                 <h2>${Card.onderwerp}</h2>
                 <p> ${Card.wat}</p>
                 <h1> ${Card.niveau}</h1>
-                <button>Lees meer</button>
-                <img src= "" width= "60px"</img>
+                <img src="css/hu_logo.png" width= "75px"</img>
+                <button id="modalBtn">Lees meer</button>
             </ul>
         `;
         })
         .join('');
         cardListRecommended.innerHTML = htmlString;
+        cardListUploaded.innerHTML = htmlString;
 };
 loadCharacters();
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalBtn");
+    
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
