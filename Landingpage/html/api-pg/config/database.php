b92348b901017788c73853b1cd614ce13ad918ea
lucius02 <54938206+lucius02@users.noreply.github.com>
2020-12-07 10:35:39 +0100

    Add files via upload

---
+++ Landingpage/html/api-pg/config/database.php
@@ -0,0 +1,27 @@
+<?php
+class Database{
+  
+    // specify your own database credentials
+    private $host = "localhost";
+    private $port = "5432";
+    private $db_name = "EBilly";
+    private $username = "postgres";
+    private $password = "WelKom7993";
+    public $conn;
+  
+    // get the database connection
+    public function getConnection(){
+  
+        $this->conn = null;
+  
+        try{
+            $this->conn = new PDO("pgsql: host=" . $this->host . "; port=" . $this->port . " dbname=" . $this->db_name . " user=" . $this->username . " password=" . $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+  
+        return $this->conn;
+    }
+}
+?>
